e7ad813f574e03f5c15a2c8ed11c1f4158170b0c
Paolo Carusi <carusipaolo@gmail.com>
2022-11-10 10:17:08 -0300

    Eliminacion de archivos y copias de desarrollo redundantes

--- js/script.js
+++ js/script.js
@@ -1,157 +1,176 @@
 // Unit converter application
 
 // Global variables and arrays definition
-const categories = new Array();
-const units = new Array();
-const factors = new Array();
+const categoriesList = document.querySelector('#categories_list');
+(inputUnit = document.querySelector('#input_list')),
+    (outputUnit = document.querySelector('#output_list')),
+    (inputValue = document.querySelector('#input_value')),
+    (outputValue = document.querySelector('#output_value')),
+    (invertBtn = document.querySelector('#invertBtn'));
 
-categories[0] = 'Distancia';
-units[0] = new Array(
-    'Kilometros',
-    'Metros',
-    'Centimetros',
-    'Milimetros',
-    'Milla',
-    'Yarda',
-    'Pie',
-    'Pulgada'
-);
-factors[0] = new Array(1, 1000, 100000, 1000000, 0.6214, 1093.6, 3280.8, 39370);
-
-categories[1] = 'Peso';
-units[1] = new Array('Kilogramos', 'Gramos', 'Miligramos', 'Libra', 'Onza');
-factors[1] = new Array(1, 1000, 1000000, 2.2046, 35.274);
-
-categories[2] = 'Temperatura';
-units[2] = new Array('Celsius', 'Fahrenheit', 'Kelvin');
-factors[2] = new Array(1, 0.555555555555, 1);
-tempIncrement = new Array(0, 32, 273.15);
-
-categories[3] = 'Velocidad';
-units[3] = new Array(
-    'Kilometros por hora',
-    'Metros por segundo',
-    'Millas por hora'
-);
-factors[3] = new Array(1, 0.2778, 0.6214);
-
-// Function to update the categories select list with the categories array
-function updateCategories() {
-    let select = document.getElementById('categories_list');
-    for (let i = 0; i < categories.length; i++) {
-        let opt = document.createElement('option');
-        opt.value = i;
-        opt.innerHTML = categories[i];
-        select.appendChild(opt);
-    }
-}
-
-// Function to update the units input and output lists depending on the category selected from the categories array, must use the same index as the categories array data.
-function updateUnits() {
-    let category = document.getElementById('categories_list').value;
-    let select_from = document.getElementById('input_list');
-    let select_to = document.getElementById('output_list');
-    select_from.innerHTML = '';
-    select_to.innerHTML = '';
-    for (let i = 0; i < units[category].length; i++) {
-        let opt_from = document.createElement('option');
-        let opt_to = document.createElement('option');
-        opt_from.value = i;
-        opt_from.innerHTML = units[category][i];
-        opt_to.value = i;
-        opt_to.innerHTML = units[category][i];
-        select_from.appendChild(opt_from);
-        select_to.appendChild(opt_to);
-    }
-}
-
-// Convert the input value to the output value
-function convert() {
-    let category = document.getElementById('categories_list').value;
-    let input = document.getElementById('input_list').value;
-    let output = document.getElementById('output_list').value;
-    let input_value = document.getElementById('input_value').value;
-    let output_value = document.getElementById('output_value');
-    let base_unit = input_value / factors[category][input];
-    output_value.value = base_unit * factors[category][output];
-    if (category == 2) {
-        output_value.value =
-            parseFloat(output_value.value) + parseFloat(tempIncrement[output]);
-    }
-    // Round the result to 2 decimals
-    output_value.value = parseFloat(output_value.value).toFixed(2);
-}
-
-// Function to save data to local storage if checkbox is checked
-function saveData() {
-    let checkbox = document.getElementById('datos').checked;
-    let category = document.getElementById('categories_list').value;
-    let input = document.getElementById('input_list').value;
-    let output = document.getElementById('output_list').value;
-    let value = document.getElementById('input_value').value;
-    if (checkbox) {
-        localStorage.setItem('category', category);
-        localStorage.setItem('input', input);
-        localStorage.setItem('output', output);
-        localStorage.setItem('input_value', value);
-        localStorage.setItem('output_value', result);
-    }
-}
-
-// Convert the arrays and variables in json format and save them in local storage
-function saveJson() {
-    let json = JSON.stringify({
-        categories: categories,
-        units: units,
-        factors: factors,
-        tempIncrement: tempIncrement,
+// Update the categories select list with the categories from the json file data without including duplicates
+const updateCategories = async () => {
+    const response = await fetch('./js/data.json');
+    const data = await response.json();
+    const categories = data.map((item) => item.category);
+    const uniqueCategories = [...new Set(categories)];
+    uniqueCategories.forEach((category) => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.innerHTML = category;
+        categoriesList.appendChild(option);
     });
-    localStorage.setItem('json', json);
+};
+
+// Update the units select lists with the units from the json file data depending on the selected category. The default units for the category "Longitud" will be "Kilometros" and "Millas", for the category "Peso" will be "Kilogramos" and "Libras", for the category "Temperatura" will be "Celsius" and "Fahrenheit" and for the category "Velocidad" will be "Kilometros por hora" and "Millas por hora".
+const updateUnits = async () => {
+    const response = await fetch('./js/data.json');
+    const data = await response.json();
+    const category = categoriesList.value;
+    const categoryData = data.filter((item) => item.category === category);
+    inputUnit.innerHTML = '';
+    outputUnit.innerHTML = '';
+    categoryData.forEach((unit) => {
+        const optionFrom = document.createElement('option');
+        const optionTo = document.createElement('option');
+        optionFrom.value = unit.unit;
+        optionFrom.innerHTML = unit.unit;
+        optionTo.value = unit.unit;
+        optionTo.innerHTML = unit.unit;
+        inputUnit.appendChild(optionFrom);
+        outputUnit.appendChild(optionTo);
+    });
+    category === 'Longitud'
+        ? ((inputUnit.value = 'Kilometros'), (outputUnit.value = 'Millas'))
+        : category === 'Peso'
+        ? ((inputUnit.value = 'Kilogramos'), (outputUnit.value = 'Libras'))
+        : category === 'Temperatura'
+        ? ((inputUnit.value = 'Celsius'), (outputUnit.value = 'Fahrenheit'))
+        : category === 'Velocidad'
+        ? ((inputUnit.value = 'Kilometros por hora'),
+          (outputUnit.value = 'Millas por hora'))
+        : null;
+};
+
+// Function to convert the input value to the output value depending on the selected units and the factors from the json file data. If the category is "Temperatura", the increment should be added from the "Increment" property of the json file data. The output value will be rounded to 4 decimals if it is a decimal and the output value will be updated with the converted value in the id "output_value". The input must be converted to a base unit before the conversion and then converted to the output unit and considering the increment if the category is "Temperatura".
+function convertUnits() {
+    fetch('./js/data.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const category = categoriesList.value;
+            const categoryData = data.filter(
+                (item) => item.category === category
+            );
+            const inputUnitData = categoryData.filter(
+                (item) => item.unit === inputUnit.value
+            );
+            const outputUnitData = categoryData.filter(
+                (item) => item.unit === outputUnit.value
+            );
+            const inputFactor = inputUnitData[0].factor;
+            const outputFactor = outputUnitData[0].factor;
+            const increment = outputUnitData[0].increment;
+            const inputBaseValue = inputValue.value / inputFactor;
+            const outputBaseValue =
+                category === 'Temperatura'
+                    ? inputBaseValue * outputFactor + increment
+                    : inputBaseValue * outputFactor;
+            const outputValueRounded = outputBaseValue.toFixed(4);
+            outputValue.value = outputValueRounded;
+        });
 }
 
-// Save the json data in local storage if checkbox is checked when the convert button is clicked
-document.getElementById('convertBtn').addEventListener('click', function () {
-    saveData();
-});
-
-// Clear the input values and execute the functions when the page is loaded
-window.onload = function () {
-    document.getElementById('input_value').value = '';
-    document.getElementById('output_value').value = '';
-    updateCategories();
+// On category change the units select lists will be updated
+categoriesList.addEventListener('change', () => {
     updateUnits();
-};
-
-// Execute the functions when the category is changed in the categories list
-document
-    .getElementById('categories_list')
-    .addEventListener('change', function () {
-        updateUnits();
-    });
-
-// Convert when the "convertBtn" button is clicked
-document.getElementById('convertBtn').addEventListener('click', function () {
-    convert();
+    inputValue.value = '';
+    outputValue.value = '';
 });
 
-// Clear input and output values when page is reloaded
-window.onbeforeunload = function () {
-    document.getElementById('input_value').value = '';
-    document.getElementById('output_value').value = '';
-};
+// When the input value changes the output value will be updated
+inputValue.addEventListener('input', () => {
+    convertUnits();
+});
 
-// Function to clear all data from local storage when checkbox is unchecked
-document.getElementById('datos').addEventListener('change', function () {
-    if (!document.getElementById('datos').checked) {
-        clearAll();
+// When the input unit changes the output value will be updated
+inputUnit.addEventListener('change', () => {
+    convertUnits();
+});
+
+// When the output unit changes the output value will be updated
+outputUnit.addEventListener('change', () => {
+    convertUnits();
+});
+
+// When the id "invertBtn" is clicked, the values of the input and output units will be inverted
+invertBtn.addEventListener('click', () => {
+    const temp = inputUnit.value;
+    inputUnit.value = outputUnit.value;
+    outputUnit.value = temp;
+    inputValue.value = outputValue.value;
+    outputValue.value = inputValue.value;
+    convertUnits();
+});
+
+// If the input value is not a number, show an alert message saying that the user must enter a number
+inputValue.addEventListener('input', () => {
+    if (isNaN(inputValue.value)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Por favor ingrese un número válido',
+        });
+        inputValue.value = '';
+        outputValue.value = '';
     }
 });
 
-// Clear input when the input and output box is clicked after conversion
-document.getElementById('input_value').addEventListener('click', function () {
-    document.getElementById('input_value').value = '';
-    document.getElementById('output_value').value = '';
+// If the checkbox with the id "datos" is checked, save the input value, input unit, output unit and output value in the local storage and show an alert message saying that de data has been saved successfully
+const datos = document.querySelector('#datos');
+datos.addEventListener('change', () => {
+    if (datos.checked) {
+        localStorage.setItem('category', categoriesList.value);
+        localStorage.setItem('inputValue', inputValue.value);
+        localStorage.setItem('inputUnit', inputUnit.value);
+        localStorage.setItem('outputUnit', outputUnit.value);
+        localStorage.setItem('outputValue', outputValue.value);
+        Swal.fire({
+            icon: 'success',
+            title: 'Datos guardados',
+            text: 'Sus datos han sido guardados exitosamente',
+        });
+    }
 });
 
-// Clear checkbox if the page is reloaded
-document.getElementById('datos').checked = false;
+// On window load the categories and units select lists will be updated. If there is data in the local storage, show an alert saying that there is data saved in the browser's memory and ask to load, if the user clicks yes the input value, input unit, output unit and output value will be updated with the data from the local storage, and if it clicks no the data from the local storage will be deleted
+window.onload = () => {
+    // Reset the input and output values
+    inputValue.value = '';
+    outputValue.value = '';
+    if (localStorage.getItem('inputValue')) {
+        Swal.fire({
+            title: 'Hay información guardada en el navegador, ¿desea cargarla?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: `Si`,
+            denyButtonText: `No`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                updateCategories();
+                updateUnits();
+                categoriesList.value = localStorage.getItem('category');
+                inputValue.value = localStorage.getItem('inputValue');
+                inputUnit.value = localStorage.getItem('inputUnit');
+                outputUnit.value = localStorage.getItem('outputUnit');
+                outputValue.value = localStorage.getItem('outputValue');
+            } else if (result.isDenied) {
+                localStorage.clear();
+                updateCategories();
+                updateUnits();
+            }
+        });
+    } else {
+        updateCategories();
+        updateUnits();
+    }
+};
